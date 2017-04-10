4fb7fa9f747ad44e307b435afa9f88186a5fd95e
Levi-PC\Levi <levi820310@gmail.com>
2017-04-10 17:16:53 +0800

    LoginPage
    push Login View and Controller and ViewModel

---
+++ Lottery/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Lottery.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Lottery.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new UserViewModel();
+            if(Request.Cookies["User"] != null) 
+            {
+                model = JsonConvert.DeserializeObject<UserViewModel>(Request.Cookies["User"]);
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Index(UserViewModel model, bool RememberMe)
+        {
+            if(model.Account == "admin" && model.Password == "123")
+            {
+                if(RememberMe)
+                {
+                    var userJson = JsonConvert.SerializeObject(new {Account = model.Account, Password = model.Password});
+                    var cookies = new CookieOptions();
+                    cookies.Expires = DateTime.Now.AddYears(10);
+                    Response.Cookies.Append("User", userJson, cookies);
+                }
+                else
+                {
+                    Response.Cookies.Delete("User");
+                }
+                return RedirectToAction("About");
+            } 
+            return View(new UserViewModel());
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
